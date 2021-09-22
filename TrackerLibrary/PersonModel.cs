cce44dac9929598272629463b199ee99bf7a8835
xkalf <68588557+xkalf@users.noreply.github.com>
2021-09-22 12:58:08 +0800

    test

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TrackerLibrary
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string CellphoneNumber { get; set; }
+    }
+}
---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName { get; set; }
+    }
+}
